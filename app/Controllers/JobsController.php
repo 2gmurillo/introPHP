1d25685732b9b7a1e6bc39ee05c553529fd576c1
2gmurillo <2gmurillo@gmail.com>
2020-05-31 12:00:21 -0500

    Validaciones

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,23 +3,41 @@
 namespace App\Controllers;
 
 use App\Models\{Job};
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController
 {
     public function getAddJobAction($request)
     {
+        $responseMessage = null;
         if ($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
-            $job = new Job();
-            $job->company = $postData['company'];
-            $job->title = $postData['title'];
-            $job->picture = $postData['picture'];
-            $job->months = $postData['months'];
-            $job->function1 = $postData['function1'];
-            $job->function2 = $postData['function2'];
-            $job->function3 = $postData['function3'];
-            $job->save();
+            $jobValidator = v::key('company', v::stringType()->notEmpty())
+                ->key('title', v::stringType()->notEmpty())
+                ->key('picture', v::stringType()->notEmpty())
+                ->key('months', v::numericVal()->notEmpty())
+                ->key('function1', v::stringType()->notEmpty())
+                ->key('function2', v::stringType()->notEmpty())
+                ->key('function3', v::stringType()->notEmpty());
+
+            try {
+                $jobValidator->assert($postData);
+                $job = new Job();
+                $job->company = $postData['company'];
+                $job->title = $postData['title'];
+                $job->picture = $postData['picture'];
+                $job->months = $postData['months'];
+                $job->function1 = $postData['function1'];
+                $job->function2 = $postData['function2'];
+                $job->function3 = $postData['function3'];
+                // $job->save();
+                $responseMessage = '¡Trabajo guardado exitosamente!';
+            } catch (\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
         }
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', [
+            'responseMessage' => $responseMessage
+        ]);
     }
 }
--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -3,19 +3,35 @@
 namespace App\Controllers;
 
 use App\Models\{Project};
+use Respect\Validation\Validator as v;
 
 class ProjectsController extends BaseController
 {
     public function getAddProjectAction($request)
     {
+        $responseMessage = null;
+
+
         if ($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
-            $project = new Project();
-            $project->company = $postData['company'];
-            $project->title = $postData['title'];
-            $project->picture = $postData['picture'];
-            $project->save();
+            $projectValidator = v::key('company', v::stringType()->notEmpty())
+                ->key('title', v::stringType()->notEmpty())
+                ->key('picture', v::stringType()->notEmpty());
+
+            try {
+                $projectValidator->assert($postData);
+                $project = new Project();
+                $project->company = $postData['company'];
+                $project->title = $postData['title'];
+                $project->picture = $postData['picture'];
+                // $project->save();
+                $responseMessage = '¡Proyecto guardado exitosamente!';
+            } catch (\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
         }
-        return $this->renderHTML('addProject.twig');
+        return $this->renderHTML('addProject.twig', [
+            'responseMessage' => $responseMessage
+        ]);
     }
 }
